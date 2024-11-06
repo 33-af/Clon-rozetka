868adf1584e0846eb389d5b0c9d9ed4f948b652d
Mykhail <gorgefraggggg@gmail.com>
2024-11-06 19:47:28 +0300

    change back

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
+
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = 5000;
 
